cbaa83eb53ad185038c541566c4ea1ddf4edb9ac
tprei <thiagopreischadt@gmail.com>
2026-09-22 19:02:14 +0100

    Restyle render-markdown: block headings and bordered code
    Drop heading icons and signs in favor of block-width headings, and give
    code blocks a thin border and block width for a cleaner rendered look.

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -1,13 +1,28 @@
 return {
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-		---@module 'render-markdown'
-		---@type render.md.UserConfig
-		opts = {
-			heading = {
-				icons = { "# ", "## ", "### ", "#### ", "##### ", "###### " },
-			},
-		},
-	},
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            heading = {
+                enabled = true,
+                sign = false,
+                icons = { "", "", "", "", "", "" },
+                width = "block",
+            },
+
+            code = {
+                sign = false,
+                width = "block",
+                border = "thin",
+            },
+
+
+            bullet = {
+                enabled = true,
+            },
+        },
+    },
 }
