888f61ca7d67ae5ae41ac7b38cca69598aebbcdb
tprei <thiagopreischadt@gmail.com>
2026-09-24 10:24:55 +0100

    Paste clipboard images into ssh panes via remote upload

--- tmux/scripts/tmux-paste-image.sh
+++ tmux/scripts/tmux-paste-image.sh
@@ -37,5 +37,43 @@ if [[ ! -s "$wsl_file" ]]; then
     exit 0
 fi
 
-tmux send-keys -t "$pane" -l "$wsl_file"
-tmux display-message "tmux-paste-image: $wsl_file"
+ssh_host() {
+    if [[ -n "${TMUX_PASTE_IMAGE_HOST:-}" ]]; then
+        printf '%s' "$TMUX_PASTE_IMAGE_HOST"
+        return
+    fi
+    local pane_pid fg_pid
+    pane_pid=$(tmux display-message -p -t "$pane" '#{pane_pid}')
+    fg_pid=$(ps -o tpgid= -p "$pane_pid" | tr -d ' ')
+    local -a argv
+    read -r -a argv < <(ps -o args= -p "$fg_pid")
+    [[ "$(basename "${argv[0]:-}")" == ssh ]] || return 1
+    local i=1
+    while ((i < ${#argv[@]})); do
+        local arg="${argv[i]}"
+        case "$arg" in
+        --) printf '%s' "${argv[i + 1]:-}"; return ;;
+        -[BbcDEeFIiJLlmOoPpQRSWw]) i=$((i + 2)) ;;
+        -*) i=$((i + 1)) ;;
+        *) printf '%s' "$arg"; return ;;
+        esac
+    done
+    return 1
+}
+
+target_path="$wsl_file"
+if [[ "$(tmux display-message -p -t "$pane" '#{pane_current_command}')" == ssh ]]; then
+    if ! host=$(ssh_host) || [[ -z "$host" ]]; then
+        tmux display-message "tmux-paste-image: can't find ssh host; set TMUX_PASTE_IMAGE_HOST"
+        exit 0
+    fi
+    name=$(basename "$wsl_file")
+    remote_cmd='d="$HOME/.cache/tmux-paste-image" && mkdir -p "$d" && cat > "$d/'"$name"'" && printf %s "$d/'"$name"'"'
+    if ! target_path=$(ssh -o BatchMode=yes "$host" "$remote_cmd" < "$wsl_file"); then
+        tmux display-message "tmux-paste-image: upload to $host failed"
+        exit 0
+    fi
+fi
+
+tmux send-keys -t "$pane" -l "$target_path"
+tmux display-message "tmux-paste-image: $target_path"
